d337a279e40230bc207b894f0ea3881c01255905
Ivan Martyanov <etoprostobaton@gmail.com>
2023-07-15 16:23:03 +0700

    xd

--- request-manager/src/main/java/com/example/requestmanager/controller/BatchController.java
+++ request-manager/src/main/java/com/example/requestmanager/controller/BatchController.java
@@ -32,6 +32,6 @@ public class BatchController {
         User user = jwtDecoder.extractUser(authHeader.substring(7));
         batchService.saveFromCsv(user, file);
 
-        return "gowno";
+        return "users have been saved";
     }
 }
